0fd1628ac3a068900b78ec490cb891b3fee96e12
Wilma <wilmareistad@icloud.com>
2026-09-09 14:42:08 +0200

    added class on button

--- src/components/ConfiguratorPanel/PriceBar.jsx
+++ src/components/ConfiguratorPanel/PriceBar.jsx
@@ -2,7 +2,7 @@ export default function PriceBar() {
   return (
     <div className="price-bar">
       <span className="price">00000€</span>
-      <button className="add-to-cart">add to cart</button>
+      <button className="mainBtn">add to cart</button>
     </div>
   )
 }
