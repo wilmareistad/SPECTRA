a6679582b3351c020c20a95d00c8e1be45d6af01
Wilma <wilmareistad@icloud.com>
2026-09-10 08:55:08 +0200

    css for panel

--- src/components/ConfiguratorPanel/ConfiguratorPanel.jsx
+++ src/components/ConfiguratorPanel/ConfiguratorPanel.jsx
@@ -3,15 +3,26 @@ import LensTypePicker from './LensTypePicker'
 import SizePicker from './SizePicker'
 import AttachmentsGrid from './AttachmentsGrid'
 import PriceBar from './PriceBar'
+import styles from './ConfiguratorPanel.module.css'
 
 export default function ConfiguratorPanel({ colour, onColourChange }) {
   return (
-    <div className="configurator-panel">
-      <ColourPicker value={colour} onChange={onColourChange} />
-      <LensTypePicker />
-      <SizePicker />
-      <AttachmentsGrid />
-      <PriceBar />
+    <div className={styles.panel}>
+      <div className={styles.section}>
+        <ColourPicker value={colour} onChange={onColourChange} />
+      </div>
+      <div className={styles.section}>
+        <LensTypePicker />
+      </div>
+      <div className={styles.section}>
+        <SizePicker />
+      </div>
+      <div className={styles.section}>
+        <AttachmentsGrid />
+      </div>
+      <div className={styles.priceSection}>
+        <PriceBar />
+      </div>
     </div>
   )
 }
--- src/components/ConfiguratorPanel/PriceBar.jsx
+++ src/components/ConfiguratorPanel/PriceBar.jsx
@@ -1,6 +1,6 @@
 export default function PriceBar() {
   return (
-    <div className="price-bar">
+    <div className="priceBar">
       <span className="price">00000€</span>
       <button className="mainBtn">add to cart</button>
     </div>
