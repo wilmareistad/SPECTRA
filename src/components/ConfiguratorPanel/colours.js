35e97252e055e7773b7fe35b13d5fa612c633985
Wilma <wilmareistad@icloud.com>
2026-09-11 11:39:36 +0200

    fixed naming and css

--- src/components/ConfiguratorPanel/colours.js
+++ src/components/ConfiguratorPanel/colours.js
@@ -6,7 +6,7 @@ export const COLOURS = [
 ]
 
 export const LENSCOLOURS = [
+  { name: 'UV protection', hex: '#d88945', image: '/UVLens.png' },
   { name: 'original', hex: '#d6ecff' },
-  { name: 'orange', hex: '#d88945', image: '/UVLens.png' },
   { name: 'brown', hex: '#76543d' },
 ]
