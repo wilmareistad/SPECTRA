8a032b101cef6db75c423fb69e7cb11a99975a1c
Wilma <wilmareistad@icloud.com>
2026-09-16 11:25:59 +0200

    fixed camere

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -5,7 +5,7 @@ import { Vector3 } from 'three'
 import Model from './Model'
 
 
-const DEFAULT_CAMERA_POSITION = [-2, 1.5, 4.8]
+const DEFAULT_CAMERA_POSITION = [-1.78, 1.25, 4.28]
 const DEFAULT_CAMERA_TARGET = [0, 0.1, 0]
 const MODEL_SCALES = {
     Small: 0.98,
